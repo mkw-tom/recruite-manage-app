2e571f5a5581fc21ce5ed3b87c1473450a9d9072
Tomu Maekawa <tomu.m1107@icloud.com>
2024-06-26 01:17:54 +0900

    テーブルの大まかなデザインを実装した。

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
+import Home from "./pages/Home"
+import './index.css'
 
-import Header from './component/common/Header'
 
 const App = () => {
   return (
     <div>
-      <Header />
+      <Home />
     </div>
   )
 }
---
+++ src/pages/Home.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Header from '../component/common/Header'
+import Table from '../component/scheduleTools/Table';
+import "../index.css"
+
+const Home = () => {
+  return (
+    <div className='w-full h-auto '>
+      <Header />
+      <div className=' flex flex-col w-full mt-24 h-auto'>
+        <div className=''>
+          追加
+        </div>
+        <Table />
+      </div>
+    </div>
+  )
+}
+
+export default Home
