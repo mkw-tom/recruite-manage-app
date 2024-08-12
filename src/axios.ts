019a83662d594f6c446a94619d96315b95320eed
Tomu Maekawa <tomu.m1107@icloud.com>
2024-08-12 12:54:08 +0900

    本番環境用のURLを設定した

--- src/axios.ts
+++ src/axios.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosInstance } from 'axios';
 
 export const api: AxiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: import.meta.env.VITE_API_URL || import.meta.env.VITE_LOCAL_API_URL,
   headers: { 'Content-Type': 'application/json' },
 });
