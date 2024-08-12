6c14e6ca282b161ce233df10f053eeb6a5c28825
Tomu Maekawa <tomu.m1107@icloud.com>
2024-08-12 14:56:27 +0900

    環境変数の呼び出しミスを修正

--- src/axios.ts
+++ src/axios.ts
@@ -2,7 +2,7 @@ import axios, { AxiosInstance } from 'axios';
 
 export const api: AxiosInstance = axios.create({
   baseURL:
-    process.env.NODE_ENV === 'production'
+    import.meta.env.NODE_ENV === 'production'
       ? import.meta.env.VITE_API_URL
       : import.meta.env.VITE_LOCAL_API_URL,
   headers: { 'Content-Type': 'application/json' },
