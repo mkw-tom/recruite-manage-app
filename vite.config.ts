c7abfaef17de8fca77a2d154863b7d4b046f6757
Tomu Maekawa <tomu.m1107@icloud.com>
2024-08-13 07:38:20 +0900

    fix vite.config 3

--- vite.config.ts
+++ vite.config.ts
@@ -4,17 +4,4 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3002, // ローカル開発用のポート
-    proxy: {
-      '/api': {
-        target: process.env.VITE_API_URL, // 環境変数からAPI URLを指定
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
-  define: {
-    'process.env': process.env, // process.envで環境変数を使用可能に
-  },
 });
